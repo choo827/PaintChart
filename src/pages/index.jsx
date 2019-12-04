88fcc5aa6f978ec6c6882230e777e43b510b8b84
choo827 <choo827@naver.com>
2019-12-04 16:20:12 +0900

    styled-components로 변경

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,9 +2,8 @@ import React, { useState, useMemo } from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import file from '../data/themes.json';
-import '../styles/index.css';
 
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import {
   ItemBoxContainer,
   ItemBox,
@@ -41,23 +40,105 @@ const IndexPage = () => {
     <Layout>
       <SEO
         title="PaintChart"/>
-      <div className="slider-wrapper">
+      <SliderWrapper>
         Make your own
-        <div className="slider">
-          <div className="slider-text1">TradingView</div>
-          <div className="slider-text2">Bitcoin ⛓</div>
-          <div className="slider-text3">Stock 🔺</div>
-        </div>
+        <Slider>
+          <SlideText1 active>TradingView</SlideText1>
+          <SlideText2>Bitcoin ⛓</SlideText2>
+          <SlideText3>Stock 🔺</SlideText3>
+        </Slider>
         chart.
-      </div>
+      </SliderWrapper>
       <StyledContainer>{renderFile}</StyledContainer>
     </Layout>
   );
 };
 
+const roll = keyframes`
+  0% {
+    margin-top: -35rem;
+  }
+  5% {
+    margin-top: -17.5rem;
+  }
+  /* 5% delay */
+  33% {
+    margin-top: -17.5rem;
+  }
+  38% {
+    margin-top: -8.75rem;
+  }
+  /* 5% delay */
+  66% {
+    margin-top: -8.75rem;
+  }
+  71% {
+    margin-top: 0;
+  }
+  /* 5% delay */
+  100% {
+    margin-top: 0;
+  }
+`;
+
 const StyledContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
 `;
+const SliderWrapper = styled.div`
+  margin-top: 16rem;
+  margin-bottom: 16rem;
+  font-size: 3rem;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Slider = styled.div`
+  height: 4.375rem;
+  overflow: hidden;
+`;
+
+const SlideText1 = styled.div`
+  background: #2196f3;
+  animation: ${roll} 7s linear infinite;
+  display: flex;
+   align-items: center;
+   justify-content: center;
+   height: 100%;
+   color: #fff;
+   margin-bottom: 4.375rem;
+   padding: 2px 15px;
+   text-align: center;
+   box-sizing: border-box;
+`;
+
+const SlideText2 = styled.div`
+  background: #FF9500;
+  display: flex;
+   align-items: center;
+   justify-content: center;
+   height: 100%;
+   color: #fff;
+   margin-bottom: 4.375rem;
+   padding: 2px 15px;
+   text-align: center;
+   box-sizing: border-box;
+`;
+
+const SlideText3 = styled.div`
+  background: tomato;
+  display: flex;
+   align-items: center;
+   justify-content: center;
+   height: 100%;
+   color: #fff;
+   margin-bottom: 4.375rem;
+   padding: 2px 15px;
+   text-align: center;
+   box-sizing: border-box;
+`;
+
+
 
 export default IndexPage;
