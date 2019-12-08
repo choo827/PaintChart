0ad680e43962f390bbb7641a54f0565a291fb23a
choo827 <choo82742@gmail.com>
2019-12-08 21:49:05 +0900

    GTM 인식 안되는 오류해결

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,17 +40,16 @@ module.exports = {
 
         // Include GTM in development.
         // Defaults to false meaning GTM will only be loaded in production.
-        includeInDevelopment: false,
+        includeInDevelopment: true,
 
         // datalayer to be set before GTM is loaded
         // should be an object or a function that is executed in the browser
         // Defaults to null
-        defaultDataLayer: { platform: "gatsby" },
-
-        // Specify optional GTM environment details.
-        gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
-        gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
-        dataLayerName: "YOUR_DATA_LAYER_NAME",
+        defaultDataLayer: function() {
+          return {
+            pageType: window.pageType,
+          }
+        },
       },
     },
     {
